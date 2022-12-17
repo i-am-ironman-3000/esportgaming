7a9553d6da4ad5315cfaf7783768ddf7c7c7d4b7
i-am-ironman-3000 <misravaibhav752@gmail.com>
2022-12-17 12:39:29 +0530

    initial code

---
+++ src/main/java/com/esport/gamingworld/GamingworldApplication.java
@@ -0,0 +1,13 @@
+package com.esport.gamingworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esport/gamingworld/GamingworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esport.gamingworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
